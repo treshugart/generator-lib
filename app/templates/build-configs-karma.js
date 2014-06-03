80dbe6fc063d08c369be9b8b1e0d7c3d20924819
Trey Shugart <treshugart@gmail.com>
2014-06-03 17:21:44 +1000

    Dynamic hostname and ports for tests.

--- app/templates/build-configs-karma.js
+++ app/templates/build-configs-karma.js
@@ -1,23 +1,29 @@
-module.exports = {
-  options: {
-    browsers: ['PhantomJS'],
-    files: [
-      'src/<%= libraryName %>.js',
-      'test/<%= libraryName %>.js'
-    ],
-    frameworks: [
-      'chai',
-      'mocha'
-    ],
-    plugins: [
-      'karma-chai',
-      'karma-mocha',
-      'karma-phantomjs-launcher'
-    ],
-    singleRun: true
-  },
-  cli: {},
-  http: {
-    singleRun: false
-  }
+module.exports = function (grunt) {
+  return {
+    options: {
+      hostname: grunt.option('host') || 'localhost',
+      port: grunt.option('port') || '9876',
+      browsers: ['PhantomJS'],
+      files: [
+        'src/<%= libraryName %>.js',
+        'test/<%= libraryName %>.js'
+      ],
+      frameworks: [
+        'chai',
+        'mocha',
+        'sinon'
+      ],
+      plugins: [
+        'karma-chai',
+        'karma-mocha',
+        'karma-phantomjs-launcher',
+        'karma-sinon'
+      ],
+      singleRun: true
+    },
+    cli: {},
+    http: {
+      singleRun: false
+    }
+  };
 };
